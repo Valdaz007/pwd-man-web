80b285a34a98fb80297f741e1b6bc03f3a78ed6f
Valdaz007 <59840456+Valdaz007@users.noreply.github.com>
2024-01-28 18:18:30 +1000

    Add: Sign Up Page Init-Layout

--- signup.php
+++ signup.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="./inc/css/index.css">
-    <title>PWD-MAN</title>
+    <link rel="stylesheet" href="./inc/css/signup.css">
+    <title>Sign Up</title>
 </head>
 <body>
     <header>
@@ -15,11 +15,14 @@
     <main>
         <div class="login-form-wrapper">
             <form class="form-control" action="#" method="post">
-                <h2 class="mt-2">Login</h2>
+                <h2 class="mt-2">Sign Up</h2>
+                <input class="form-control mb-2" type = "text" name = "uemail" placeholder="Email" required>
+                <input class="form-control mb-2" type = "text" name = "fname" placeholder="First Name" required>
+                <input class="form-control mb-2" type = "text" name = "lname" placeholder="Last Name" required>
                 <input class="form-control mb-2" type = "text" name = "uname" placeholder="Username" required>
                 <input class="form-control mb-2" type = "password" name = "upwd" placeholder="Password" required>
                 <input class="btn btn-primary" type = "submit" name = "Sumit">
-                <a href="./signup.php">I don't have an account</a>
+                <a class="btn btn-warning" href="./index.php">Cancel</a>
             </form>
         </div>
     </main>
