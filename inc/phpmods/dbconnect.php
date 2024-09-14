52f9060ca0af70cef4b981913cba7b35cb84585e
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2024-09-14 19:14:21 +1000

    Update

--- inc/phpmods/dbconnect.php
+++ inc/phpmods/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 
 function dbconxn() {
-    $conxn = mysqli_connect("srv605.hstgr.io", "u380947895_ntt_db_0823", "NTT_db_0823", "u380947895_ntt_db_0823");
+    $conxn = mysqli_connect("hostname", "username", "password", "db");
     // Check connection
     if (!$conxn) {
         die("Connection failed: " . mysqli_connect_error());
